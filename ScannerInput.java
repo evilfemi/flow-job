b69d7dd7c14f726c00eba7ade4c61b41b0ca5701
Omoope <adedoyinope@gmail.com>
2017-02-24 20:02:24 -0500

    ScannerInput.java

---
+++ ScannerInput.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class ScannerInput{
+	public static void main(Strings [] args){
+	int n =1;
+
+	Scanner input = new Scanner(System.in);
+	//compute and print out student score
+	
+	System.out.println("Please enter the number of course you took last semester");
+	
+	n = input.nextInt
+		double avg
+
+
+	
+
+	
+	}
+}	
